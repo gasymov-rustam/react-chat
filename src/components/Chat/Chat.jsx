6196ce665d9a986fa70f4892d402f2b8c6ae6466
rust_man <gasymovrustam86@yandex.com>
2021-11-17 08:46:30 +0200

    were installed dependencies

---
+++ src/components/Chat/Chat.jsx
@@ -0,0 +1,6 @@
+import styles from "./Chat.module";
+const Chat = () => {
+    return <div>Chat</div>;
+};
+
+export default Chat;
---
+++ src/components/Message/Message.jsx
@@ -0,0 +1,6 @@
+import styles from "./Message.module.css";
+const Message = () => {
+    return <div>Message</div>;
+};
+
+export default Message;
